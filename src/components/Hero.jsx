8cb02878c6bdf0ad71a2906d4709efa6c7726df4
Mahmud-Amaan <mahmud.amaan20104@gmail.com>
2024-02-13 11:51:06 +0600

    name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className=" text-[#915eff]">Maha</span>
+            Hi, I'm <span className=" text-[#915eff]">ConstSudo</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop front end websites <br className="sm:block hidden" /> and
